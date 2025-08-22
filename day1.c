f29485eff82c3738ae1b266be4edf69403eb4759
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-08-22 10:44:33 +0530

    add two variable

---
+++ day1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    int sum;
+    printf("enter the value a");
+    scanf("%d",&a);
+
+    printf("enter the value b");
+    scanf("%d",&b);
+
+    sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
