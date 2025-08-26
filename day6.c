565f0a7696e10011df66ec7e40b18a12e82fe063
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-08-26 23:50:10 +0530

    day 6 question

---
+++ day6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int number, a;
+
+    printf("enter any value a ");
+    scanf("%d", a);
+
+    if (number % 2 == 0)
+        printf("even number");
+    else
+        printf("odd number");
+
+    return 0;
+}
