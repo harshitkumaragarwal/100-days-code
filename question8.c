2d4d77cf225fba800631a5acc41d9797fd676fae
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-08-28 18:34:08 +0530

    day8 question2

---
+++ question8.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+
+    printf("enter a number");
+    scanf("%d",&a);
+
+    printf("enter b number");
+    scanf("%d",&b);
+
+    printf("enter c number");
+    scanf("%d",&c);
+
+    if(a>b && a>c){
+        printf("a is largest number %d",a);
+    } else if(b>a && b>c){
+         printf("b is largest number %d",b);
+    }
+    else
+    printf("c is largest number %d",c);
+
+    return 0;
+
+
+
+
+
+}
