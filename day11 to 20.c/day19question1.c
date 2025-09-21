e5cf3461e3c401835c6dcef277df19b763ebdeb3
harshitkumaragarwal <agarwalharshit063@gmail.com>
2025-09-21 13:40:05 +0530

    lcm number

--- day11 to 20.c/day19question1.c
+++ day11 to 20.c/day19question1.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
 int main(){
-    int a, b, temp;
+    int a, b, x, y, temp, gcd, lcm;
 
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
     while(b != 0){
         temp = b;
         b = a % b;
         a = temp;
     }
+    gcd = a;
 
-    printf("HCF (GCD) = %d", a);
+    lcm = (x * y) / gcd;
+
+    printf("LCM = %d", lcm);
 
     return 0;
 }
+
